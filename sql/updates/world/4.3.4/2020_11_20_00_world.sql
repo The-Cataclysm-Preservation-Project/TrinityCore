f2a31b67e13f707c84b80d0b3e76a561ec487523
iThorgrim-Hub <contact@ithorgrim.eu>
2020-11-20 23:36:20 +0100

    update bnet & npc

---
+++ sql/updates/world/4.3.4/2020_11_20_00_world.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `creature_trainer` VALUES (35872, 44, (SELECT gossip_menu_id FROM creature_template WHERE entry  = 35872), 0);
+INSERT IGNORE INTO `creature_trainer` VALUES (44465, 135, (SELECT gossip_menu_id FROM creature_template WHERE entry  = 44465), 0);
