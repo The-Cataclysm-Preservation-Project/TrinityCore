19d278d365897a98e5e0c9b6d7b535a1a4143d99
Ovah <dreadkiller@gmx.de>
2020-11-23 00:09:54 +0100

    Update 2020_11_20_00_world.sql

--- sql/updates/world/4.3.4/2020_11_20_00_world.sql
+++ sql/updates/world/4.3.4/2020_11_20_00_world.sql
@@ -1,5 +1,4 @@
-DELETE FROM `creature_trainer` WHERE creatureid = 35872;
-INSERT INTO `creature_trainer` VALUES (35872, 44, 10698, 0);
-
-DELETE FROM `creature_trainer` WHERE creatureid = 44465;
-INSERT INTO `creature_trainer` VALUES (44465, 135, 10698, 0);
+DELETE FROM `creature_trainer` WHERE `CreatureId` IN (35872, 44465);
+INSERT INTO `creature_trainer` (`CreatureId`, `TrainerId`, `MenuId`, `OptionIndex`) VALUES
+(35872, 44, 10698, 0),
+(44465, 135, 10698, 0);
