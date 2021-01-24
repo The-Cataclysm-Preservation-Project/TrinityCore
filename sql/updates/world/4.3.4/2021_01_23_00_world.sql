4f6b7a1f292c1affefef1fcf7e06d5d7fb3b2337
Doan Manh <doanvanmanh87@gmail.com>
2021-01-24 08:28:16 +0700

    Correct sql statement and add newline at the end of file.

--- sql/updates/world/4.3.4/2021_01_23_00_world.sql
+++ sql/updates/world/4.3.4/2021_01_23_00_world.sql
@@ -1,6 +1,6 @@
 -- Correct hunter spell trainer
 UPDATE `trainer_spell` SET `SpellID` = 86528 WHERE `TrainerID` = 40 AND `SpellId` = 87506;
-UPDATE `trainer_spell` SET `SpellID` = 79682 WHERE `TrainerID` = 40 AND `SpellId` = 93321;
+UPDATE `trainer_spell` SET `SpellID` = 93321 WHERE `TrainerID` = 40 AND `SpellId` = 79682;
 
 -- Correct shaman spell trainer
 UPDATE `trainer_spell` SET `SpellID` = 87507 WHERE `TrainerID` = 124 AND `SpellId` = 86529;
