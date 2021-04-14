0041f98780ff743c97688641c704966d4e627865
cTrinity <58204838+cTrinity@users.noreply.github.com>
2021-04-14 21:14:23 +0800

    Create 2021_04_14_00_world.sql
    DB/QUEST : fix creature Vexallus loot (id : (Heroic) 25573 (Normal) 24744) quest(id:29686)

---
+++ sql/updates/world/4.3.4/2021_04_14_00_world.sql
@@ -0,0 +1,2 @@
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `QuestRequired`, `GroupId`) VALUES ('24744', '73084', '1', '1');
+INSERT INTO `creature_loot_template` (`Entry`, `Item`, `QuestRequired`, `GroupId`) VALUES ('25573', '73084', '1', '1');
