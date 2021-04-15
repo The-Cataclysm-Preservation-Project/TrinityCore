512201c8673271ea1955cbee655b22f532e04f47
cTrinity <58204838+cTrinity@users.noreply.github.com>
2021-04-15 10:05:48 +0800

    Update 2021_04_14_00_world.sql

--- sql/updates/world/4.3.4/2021_04_14_00_world.sql
+++ sql/updates/world/4.3.4/2021_04_14_00_world.sql
@@ -1,2 +1,4 @@
+DELETE FROM `creature_loot_template` WHERE `Entry`=24744 AND `Item`=73084;
+DELETE FROM `creature_loot_template` WHERE `Entry`=25573 AND `Item`=73084;
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `QuestRequired`, `GroupId`) VALUES ('24744', '73084', '1', '1');
 INSERT INTO `creature_loot_template` (`Entry`, `Item`, `QuestRequired`, `GroupId`) VALUES ('25573', '73084', '1', '1');
