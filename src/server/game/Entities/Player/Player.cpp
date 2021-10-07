32b1ed61a3956eaad33d3f781d97e380864bf762
acidmanifesto <joshua.lee.betts@gmail.com>
2021-10-07 17:05:00 -0400

    corrected missing if statement

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -25549,16 +25549,19 @@ void Player::StoreLootItem(uint8 lootSlot, Loot* loot, GameObject* go)
 
     LootItem* item = loot->LootItemInSlot(lootSlot, this, &qitem, &ffaitem, &conditem);
 
-    if (sConfigMgr->GetBoolDefault("AOE.LOOT.enable", true))
-    {
-        //SendEquipError(EQUIP_ERR_LOOT_GONE, nullptr, nullptr); prevents error already loot from spamming
-        return;
-    }
+    if (!item)
+	{
+        if (sConfigMgr->GetBoolDefault("AOE.LOOT.enable", true))
+        {
+            //SendEquipError(EQUIP_ERR_LOOT_GONE, nullptr, nullptr); prevents error already loot from spamming
+            return;
+        }
 
-    else
-    {
-        SendEquipError(EQUIP_ERR_LOOT_GONE, nullptr, nullptr);
-        return;
+        else
+        {
+            SendEquipError(EQUIP_ERR_LOOT_GONE, nullptr, nullptr);
+            return;
+        }
     }
 
     if (!item->AllowedForPlayer(this))
