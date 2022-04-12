e0c878d1c01542f43bf5916b0c35249778cf22a8
AG <43139552+AGandrup@users.noreply.github.com>
2022-04-12 20:28:55 +0200

    Reverting some changes + combine sql files
    - Shadowmeld seems to trigger its cd correctly afterall.

--- sql/updates/world/4.3.4/2022_04_10_00_world.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_gen_shadowmeld';
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (58984, 'spell_gen_shadowmeld');
--- sql/updates/world/4.3.4/2022_04_12_00_world.sql
+++ sql/updates/world/4.3.4/2022_04_12_00_world.sql
@@ -1,3 +1,3 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_dru_pounce' OR `ScriptName`= 'spell_dru_ravage';
+DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_dru_pounce' OR `ScriptName`= 'spell_dru_ravage' OR `ScriptName`= 'spell_gen_shadowmeld';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(9005, 'spell_dru_pounce'), (6785, 'spell_dru_ravage');
+(9005, 'spell_dru_pounce'), (6785, 'spell_dru_ravage'), (58984, 'spell_gen_shadowmeld');
