e3d81216dbf602601d940ac6d125c0bf605b2b77
Ovah <dreadkiller@gmx.de>
2022-04-12 20:32:05 +0200

    Update 2022_04_12_00_world.sql

--- sql/updates/world/4.3.4/2022_04_12_00_world.sql
+++ sql/updates/world/4.3.4/2022_04_12_00_world.sql
@@ -1,3 +1,9 @@
-DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_dru_pounce' OR `ScriptName`= 'spell_dru_ravage' OR `ScriptName`= 'spell_gen_shadowmeld';
+DELETE FROM `spell_script_names` WHERE `ScriptName` IN 
+('spell_dru_pounce',
+'spell_dru_ravage',
+'spell_gen_shadowmeld');
+
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(9005, 'spell_dru_pounce'), (6785, 'spell_dru_ravage'), (58984, 'spell_gen_shadowmeld');
+(9005, 'spell_dru_pounce'),
+(6785, 'spell_dru_ravage'),
+(58984, 'spell_gen_shadowmeld');
