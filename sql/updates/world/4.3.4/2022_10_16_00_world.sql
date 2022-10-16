48490e3aacb7ad87932344fe14ce17b9c202121c
AG <43139552+AGandrup@users.noreply.github.com>
2022-10-16 14:42:39 +0200

    Create 2022_10_16_00_world.sql

---
+++ sql/updates/world/4.3.4/2022_10_16_00_world.sql
@@ -0,0 +1,2 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName`= 'spell_rog_glyph_of_blind';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (2094, 'spell_rog_glyph_of_blind');
