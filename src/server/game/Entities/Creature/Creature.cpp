7ece100da8793a873346072ff40e3bb61a51b2cf
Ovahlord <dreadkiller@gmx.de>
2025-03-10 18:26:08 +0100

    Core/Creature: removed a duplicate application of CREATURE_STATIC_FLAG_4_NO_BIRTH_ANIM

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -561,8 +561,6 @@ bool Creature::InitEntry(uint32 entry, CreatureData const* data /*= nullptr*/)
 
     _staticFlags.ApplyFlag(CREATURE_STATIC_FLAG_4_TREAT_AS_RAID_UNIT_FOR_HELPFUL_SPELLS, (cinfo->type_flags & CREATURE_TYPE_FLAG_TREAT_AS_RAID_UNIT) != 0);
 
-    m_updateFlag.NoBirthAnim = _staticFlags.HasFlag(CREATURE_STATIC_FLAG_4_NO_BIRTH_ANIM);
-
     SetNoNpcDamageBelowPctHealthValue(sObjectMgr->GetSparringHealthLimitFor(GetEntry()));
 
     return true;
