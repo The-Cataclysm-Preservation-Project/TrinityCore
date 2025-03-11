5d8b7367ef7742ebc48127b7aaebfdb933127f25
Ovahlord <dreadkiller@gmx.de>
2025-03-11 18:16:17 +0100

    DB/Creatures: removed forbidden unitFlags and unitFlags2 from database

---
+++ sql/updates/world/4.3.4/2025_03_11_01_world.sql
@@ -0,0 +1,5 @@
+UPDATE `creature_template` SET `unit_flags`= `unit_flags` & 33555264;
+UPDATE `creature` SET `unit_flags`= `unit_flags` & 33555264;
+
+UPDATE `creature_template` SET `unit_flags2`= `unit_flags2` & 67323939;
+UPDATE `creature` SET `unit_flags2`= `unit_flags2` & 67323939;
