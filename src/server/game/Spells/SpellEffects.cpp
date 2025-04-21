c550e2d29f3b78c76e5c699e86d50b3cc5630fda
Shauren <shauren.trinity@gmail.com>
2025-04-21 23:27:25 +0200

    Core/Spells: Handle SPELL_EFFECT_FORCE_CAST on spell launch instead of hit

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -640,7 +640,7 @@ void Spell::EffectTriggerMissileSpell(SpellEffIndex effIndex)
 
 void Spell::EffectForceCast(SpellEffIndex effIndex)
 {
-    if (effectHandleMode != SPELL_EFFECT_HANDLE_HIT_TARGET)
+    if (effectHandleMode != SPELL_EFFECT_HANDLE_LAUNCH_TARGET)
         return;
 
     if (!unitTarget)
