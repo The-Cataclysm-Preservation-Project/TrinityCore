89e601a4e957bd79af53e0a454382b09d6da017f
Ovahlord <dreadkiller@gmx.de>
2025-06-11 12:40:22 +0200

    DB/Misc: removed an unneeded comment from update field

--- sql/updates/world/4.3.4/2025_06_08_01_world.sql
+++ sql/updates/world/4.3.4/2025_06_08_01_world.sql
@@ -3,5 +3,3 @@ INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 (19434, 'spell_hun_aimed_shot'),
 (82928, 'spell_hun_aimed_shot'),
 (53301, 'spell_hun_explosive_shot');
-
--- dk strike dmg bonus
